e63cb3f524fc5382589747e0dc09bd5356eefe6a
patelsahil-cmd <7011246@spsd.sk.ca>
2025-01-07 11:44:37 -0600

    learned about how to Utilize the Collide2D Library:.

--- Game Mechanics Collision Detection/sketch.js
+++ Game Mechanics Collision Detection/sketch.js
@@ -33,26 +33,69 @@
 
 // point rectangle collision........................................................................  
 
-let rX = 200; let rY = 150;
-let rWidth = 300; let rHeight = 100;
-let rLeft = rX; let rTop = rY;
-let rRight = rX + rWidth; let rBottom = rTop + rHeight;
+// let rX = 200; let rY = 150;
+// let rWidth = 300; let rHeight = 100;
+// let rLeft = rX; let rTop = rY;
+// let rRight = rX + rWidth; let rBottom = rTop + rHeight;
 
 
 
-function setup() {
-     createCanvas(windowWidth, windowHeight);
-   }
+// function setup() {
+//      createCanvas(windowWidth, windowHeight);
+//    }
   
-   function draw() {
-     background(220);
-     fill(225);
-     if(mouseX > rLeft && mouseY < rRight){
-      if(mouseX > rTop && mouseY < rBottom){
-        fill (200, 54, 196);
+//    function draw() {
+//      background(220);
+//      fill(225);
+//      if(mouseX > rLeft && mouseY < rRight){
+//       if(mouseX > rTop && mouseY < rBottom){
+//         fill (200, 54, 196);
 
-      }
-     }
+//       }
+//      }
 
-     rect(rX, rY, rWidth, rHeight);
-   }
+//      rect(rX, rY, rWidth, rHeight);
+//    }
+
+// Utilizing the Collide2D Library:........................................................................
+
+// let boxX = 300; let boxY = 150;
+//  let boxLength = 300; let boxHeight = 70;
+
+
+//  // circle position will be mouseX, and mouseY
+
+//  let circleDiameter = 120;
+
+
+
+
+// function setup(){
+//   createCanvas(windowWidth, windowHeight);
+//   noCursor();
+//   rectMode(CORNER);
+//   ellipseMode(CENTER);
+
+// }
+
+// function draw(){
+//   background(220);
+
+//   // draw box
+// if(collideRectCircle(boxY, boxX, boxLength, boxHeight, mouseX, mouseY, circleDiameter)){
+//   fill(255,255,0);
+
+// }
+// else{
+//   fill(255);
+// }
+//   fill(225);
+//   rect(boxX, boxY, boxLength, boxHeight);
+
+//   // draw circle 
+// fill(225, 150);
+// ellipse(mouseX, mouseY, circleDiameter, circleDiameter);
+
+
+
+// }
